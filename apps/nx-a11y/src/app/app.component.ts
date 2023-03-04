5ec051cc29b383a771512662cabe3eff93d9a880
Sandi Barr <sandi.k.barr@gmail.com>
2023-03-04 11:20:31 -0600

    feat: base app

--- apps/nx-a11y/src/app/app.component.ts
+++ apps/nx-a11y/src/app/app.component.ts
@@ -1,11 +1,10 @@
-import { NxWelcomeComponent } from './nx-welcome.component';
 import { RouterModule } from '@angular/router';
 import { Component } from '@angular/core';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
-  selector: 'nx-a11y-workspace-root',
+  imports: [RouterModule],
+  selector: 'a11y-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
